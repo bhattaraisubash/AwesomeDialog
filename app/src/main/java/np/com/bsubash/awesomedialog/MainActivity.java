edbbd613836f8e1a0b1406a2babd465ca9ef47e2
Subash Bhattarai <mr.bhattaraisubash@gmail.com>
2018-10-29 14:36:09 +0545

    example implementation

--- app/src/main/java/np/com/bsubash/awesomedialog/MainActivity.java
+++ app/src/main/java/np/com/bsubash/awesomedialog/MainActivity.java
@@ -1,7 +1,10 @@
 package np.com.bsubash.awesomedialog;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import np.com.bsubash.awesomedialoglibrary.AwesomeAlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AwesomeAlertDialog dialog = new AwesomeAlertDialog(this, AwesomeAlertDialog.SUCCESS_TYPE);
+        dialog.setTitleText("It's Working !");
+        dialog.setConfirmText("OK");
+        dialog.setConfirmClickListener(Dialog::dismiss);
+        dialog.show();
     }
 }
