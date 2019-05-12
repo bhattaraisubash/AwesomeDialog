3579c1f0a15ac12ccd36a24fbc662c73560b1636
Subash Bhattarai <mr.bhattaraisubash@gmail.com>
2019-05-12 15:29:46 +0545

    clean up

--- app/src/main/java/np/com/bsubash/awesomedialog/MainActivity.java
+++ app/src/main/java/np/com/bsubash/awesomedialog/MainActivity.java
@@ -1,8 +1,8 @@
 package np.com.bsubash.awesomedialog;
 
 import android.app.Dialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import np.com.bsubash.awesomedialoglibrary.AwesomeAlertDialog;
 
